3705fda32d305d10c30433d55044be1da613752d
nakiasilva <nakias@student.unimelb.edu.au>
2019-10-22 02:35:56 +1100

    Uncommenting Hello world to access cloud functions

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -2,12 +2,12 @@ const functions = require('firebase-functions');
 const {Storage} = require('@google-cloud/storage');
 const os = require('os');
 const path = require('path');
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
 
 const projectId = 'chatapplication-d7a3e';
 const storage_bucket = new Storage({
