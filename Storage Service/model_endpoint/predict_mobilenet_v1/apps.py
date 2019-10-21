a68ccef0a5a657540b5ae2b18b906ba1ae13dd99
knightmatish <nitishm@student.unimelb.edu.au>
2019-10-22 02:51:50 +1100

    Removing unused CNN

--- Storage Service/model_endpoint/predict_mobilenet_v1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PredictMobilenetV1Config(AppConfig):
-    name = 'predict_mobilenet_v1'
--- Storage Service/model_endpoint/predict_mobilenet_v1/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from django.db import transaction
-
-from .models import UploadImage
-
-class ImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UploadImage
-        fields = ('name', 'image')
