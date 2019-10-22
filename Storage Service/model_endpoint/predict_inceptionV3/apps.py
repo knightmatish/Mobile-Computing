2848911d2dfc4fbde741755273b67c8e7315b091
ksundararaja <49825087+ksundararaja@users.noreply.github.com>
2019-10-22 20:48:33 +1100

    rewamping url files

--- Storage Service/model_endpoint/predict_inceptionV3/apps.py
+++ Storage Service/model_endpoint/predict_inceptionV3/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class PredictInceptionv3Config(AppConfig):
     name = 'predict_inceptionV3'
--- Storage Service/model_endpoint/predict_inceptionV3/urls.py
+++ Storage Service/model_endpoint/predict_inceptionV3/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
-
-
 from predict_inceptionV3.views import TestModel
-
 
 urlpatterns = [
 	url(r'^test$', TestModel.as_view(), name='testmodel'),
